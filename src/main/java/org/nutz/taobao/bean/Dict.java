d3aca3b37eb7342b3f41c6e0caa261f43de02fa2
Howe Chiang <howechiang@gmail.com>
2019-01-30 15:02:15 +0800

    first commit

---
+++ src/main/java/org/nutz/taobao/bean/Dict.java
@@ -0,0 +1,20 @@
+package org.nutz.taobao.bean;
+
+/**
+ * Created by Jianghao on 2018/3/5
+ *
+ * @author howechiang
+ */
+public class Dict {
+
+    /**
+     * 淘宝正式网关地址
+     */
+    public static String API_GATE = "\thttps://eco.taobao.com/router/rest";
+
+    /**
+     * 淘宝沙盒网关地址
+     */
+    public static String SANDBOX_API_GATE = "https://gw.api.tbsandbox.com/router/rest";
+
+}
